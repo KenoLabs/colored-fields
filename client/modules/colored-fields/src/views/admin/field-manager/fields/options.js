74722fdd0dba827c0da0c86a3b7cbaed9b2fb2f2
Valeriy Shamota <v.shamota@zinitsolutions.com>
2020-01-31 21:37:10 +0200

    81351

--- client/modules/colored-fields/src/views/admin/field-manager/fields/options.js
+++ client/modules/colored-fields/src/views/admin/field-manager/fields/options.js
@@ -17,8 +17,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-Espo.define('colored-fields:views/admin/field-manager/fields/colored-options', ['views/admin/field-manager/fields/options', 'lib!jscolor'], function (Dep) {
-    return Dep.extend({
+Espo.define('colored-fields:views/admin/field-manager/fields/options', ['class-replace!colored-fields:views/admin/field-manager/fields/options', 'lib!jscolor'],
+    Dep => Dep.extend({
 
         optionColors: null,
 
@@ -71,29 +71,15 @@ Espo.define('colored-fields:views/admin/field-manager/fields/colored-options', [
             }
         },
 
-        getItemHtml: function (value) {
-            let valueSanitized = this.getHelper().stripTags(value);
-            let translatedValue = this.translatedOptions[value] || valueSanitized;
-
-            translatedValue = translatedValue.replace(/"/g, '&quot;').replace(/\\/g, '&bsol;');
-
-            let valueInternal = valueSanitized.replace(/"/g, '-quote-').replace(/\\/g, '-backslash-');
-            let coloredValue = this.optionColors[value] || this.defaultColor;
-
+        getTranslationContainer(value, valueInternal, translatedValue, valueSanitized) {
+            const coloredValue = this.optionColors[value] || this.defaultColor;
             return `
-                <div class="list-group-item link-with-role form-inline" data-value="${valueInternal}">
-                    <div class="pull-left" style="width: 92%; display: inline-block; margin-bottom: 2px;">
-                        <input name="coloredValue" data-value="${valueInternal}" class="role form-control input-sm pull-right" value="${coloredValue}">
-                        <input name="translatedValue" data-value="${valueInternal}" class="role form-control input-sm pull-right" value="${translatedValue}">
-                        <div>${valueSanitized}</div>
-                    </div>
-                    <div style="width: 8%; display: inline-block;">
-                        <a href="javascript:" class="pull-right" data-value="${valueInternal}" data-action="removeValue"><span class="fas fa-times"></a>
-                    </div>
-                    <br style="clear: both;" />
+                <div class="pull-left" style="width: 92%; display: inline-block;">
+                    <input name="coloredValue" data-value="${valueInternal}" class="role form-control input-sm pull-right" value="${coloredValue}">
+                    <input name="translatedValue" data-value="${valueInternal}" class="role form-control input-sm pull-right" value="${translatedValue}">
+                    <div class="main-option">${valueSanitized}</div>
                 </div>`;
-        },
+        }
 
-    });
-
-});
+    })
+);
